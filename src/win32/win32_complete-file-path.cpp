672e32924b8f81873852768d793d13c9cf5f361b
Dr Ocelot <19556715+doktorocelot@users.noreply.github.com>
2023-11-23 08:28:56 -0500

    add consts

--- src/win32/win32_complete-file-path.cpp
+++ src/win32/win32_complete-file-path.cpp
@@ -3,12 +3,12 @@
 #include <fstream>
 #include <Shlwapi.h>
 
-static void setDirectoryPath(WCHAR *directoryPath, int size) {
+static void setDirectoryPath(WCHAR *directoryPath, const int size) {
     GetModuleFileName(nullptr, directoryPath, size);
     PathRemoveFileSpec(directoryPath);
 }
 
-void win32_setCompleteFilePath(std::wstring *dest, LPCWSTR relativeFilePath) {
+void win32_setCompleteFilePath(std::wstring *dest, const LPCWSTR relativeFilePath) {
     WCHAR directoryPath[MAX_PATH];
     setDirectoryPath(directoryPath, MAX_PATH);
     dest->append(directoryPath);
