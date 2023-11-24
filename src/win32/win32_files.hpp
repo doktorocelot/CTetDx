63b4b3e10378d061dec89398e8390039ec9aab09
Dr Ocelot <19556715+doktorocelot@users.noreply.github.com>
2023-11-24 08:18:06 -0500

    change type

--- src/win32/win32_files.hpp
+++ src/win32/win32_files.hpp
@@ -2,6 +2,6 @@
 
 #include <windows.h>
 
-bool fileExists(const wchar_t *filePath);
+bool fileExists(const WCHAR *filePath);
 
 void win32_setCompleteFilePath(WCHAR *dest, int destSize, LPCWSTR relativeFilePath);
