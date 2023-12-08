f687cdc67b1089005847d3956f5ee98b86e5b205
Dr Ocelot <19556715+doktorocelot@users.noreply.github.com>
2023-12-08 08:33:14 -0500

    Add error handling to win32_allocateMemory
    This commit refactors the memory allocation in the win32_specifics file. It now includes error handling: in case of unsuccessful memory allocation, an "Out of memory" message will be displayed and the program will be terminated to prevent further issues.

--- src/win32/win32_memory.cpp
+++ src/win32/win32_memory.cpp
@@ -1,8 +1,12 @@
 #include "win32_memory.hpp"
 #include <Windows.h>
 
-void *win32_allocateMemory(size_t amount) {
-    return VirtualAlloc(NULL, amount, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+#include "win32_kill-program.hpp"
+
+void *win32_allocateMemory(const size_t amount) {
+    const auto ptr = VirtualAlloc(nullptr, amount, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    if (ptr == nullptr) win32_killProgram(L"Out of memory.");
+    return ptr;
 }
 
 void win32_deallocateMemory(void *memory) {
