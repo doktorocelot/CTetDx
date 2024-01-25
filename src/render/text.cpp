b5dfbd876217ebbe6ef852ada38eb09cf2c00418
Dr Ocelot <19556715+doktorocelot@users.noreply.github.com>
2024-01-25 12:12:17 -0500

    change text system to use staging

--- src/render/text.cpp
+++ src/render/text.cpp
@@ -29,66 +29,65 @@ void textRenderer_init(
     text_initGlyphsFromBin(textRenderer->glyphs, rawGlyphData, dataLen, imgResolution);
 }
 
-void textRenderer_setText(TextRenderer *textRenderer, const Text **texts, const size_t *lengths, size_t elementCount) {
-    int charIndex = 0;
+void textRenderer_clearText(TextRenderer *textRenderer) {
     textRenderer->activeCharCount = 0;
-    for (int j = 0; j < elementCount; j++) {
-        const size_t len = lengths[j];
-        const Text *textList = texts[j];
-        for (int i = 0; i < len; i++) {
-            if (charIndex >= MAX_CHAR_QUADS) return;
-            
-            const Text text = textList[i];
-            
-            const char *string = text.string;
-            if (string == nullptr) continue;
-            
-            Vector2 caret = text.position;
-            const float size = text.size;
-            const TextAlignment alignment = text.alignment;
-            const int startingCharIndex = charIndex;
-            const int startingX = caret.x;
-            
-            while (*string != '\0') {
-                if (*string < ' ') {
-                    string++;
-                    continue;
-                }
-                if (*string == ' ') {
-                    caret.x += textRenderer->glyphs[' '].advance * size;
-                    string++;
-                    continue;
-                }
-                const auto [advance, triBounds, pixelOffset] = textRenderer->glyphs[*string];
-            
-                textRenderer->chars[charIndex][0].position = vector2_addToNew({triBounds.left * size, triBounds.bottom * size}, caret);
-                textRenderer->chars[charIndex][1].position = vector2_addToNew({triBounds.right * size, triBounds.bottom * size}, caret);
-                textRenderer->chars[charIndex][2].position = vector2_addToNew({triBounds.left * size, triBounds.top * size}, caret);
-                textRenderer->chars[charIndex][3].position = vector2_addToNew({triBounds.right * size, triBounds.top * size}, caret);
+}
 
-                textRenderer->chars[charIndex][0].texCoords = {pixelOffset.left, pixelOffset.bottom};
-                textRenderer->chars[charIndex][1].texCoords = {pixelOffset.right, pixelOffset.bottom};
-                textRenderer->chars[charIndex][2].texCoords = {pixelOffset.left, pixelOffset.top};
-                textRenderer->chars[charIndex][3].texCoords = {pixelOffset.right, pixelOffset.top};
-
-                caret.x += advance * size;
-            
-                textRenderer->activeCharCount++;
-                charIndex++;
+void textRenderer_stageText(TextRenderer *textRenderer, const Text *texts, const size_t count) {
+    int charIndex = textRenderer->activeCharCount;
+    for (int i = 0; i < count; i++) {
+        if (charIndex >= MAX_CHAR_QUADS) return;
+        
+        const Text text = texts[i];
+        
+        const char *string = text.string;
+        if (string == nullptr) continue;
+        
+        Vector2 caret = text.position;
+        const float size = text.size;
+        const TextAlignment alignment = text.alignment;
+        const int startingCharIndex = charIndex;
+        const int startingX = caret.x;
+        
+        while (*string != '\0') {
+            if (*string < ' ') {
                 string++;
+                continue;
             }
+            if (*string == ' ') {
+                caret.x += textRenderer->glyphs[' '].advance * size;
+                string++;
+                continue;
+            }
+            const auto [advance, triBounds, pixelOffset] = textRenderer->glyphs[*string];
+        
+            textRenderer->chars[charIndex][0].position = vector2_addToNew({triBounds.left * size, triBounds.bottom * size}, caret);
+            textRenderer->chars[charIndex][1].position = vector2_addToNew({triBounds.right * size, triBounds.bottom * size}, caret);
+            textRenderer->chars[charIndex][2].position = vector2_addToNew({triBounds.left * size, triBounds.top * size}, caret);
+            textRenderer->chars[charIndex][3].position = vector2_addToNew({triBounds.right * size, triBounds.top * size}, caret);
+
+            textRenderer->chars[charIndex][0].texCoords = {pixelOffset.left, pixelOffset.bottom};
+            textRenderer->chars[charIndex][1].texCoords = {pixelOffset.right, pixelOffset.bottom};
+            textRenderer->chars[charIndex][2].texCoords = {pixelOffset.left, pixelOffset.top};
+            textRenderer->chars[charIndex][3].texCoords = {pixelOffset.right, pixelOffset.top};
+
+            caret.x += advance * size;
             
-            if (alignment != TextAlignment_LEFT) {
-                float shift = caret.x - startingX;
-                if (alignment == TextAlignment_CENTER) shift /= 2;
-                const int total = charIndex;
-                for (int j = startingCharIndex; j < total; j++) {
-                    textRenderer->chars[j][0].position.x -= shift;
-                    textRenderer->chars[j][1].position.x -= shift;
-                    textRenderer->chars[j][2].position.x -= shift;
-                    textRenderer->chars[j][3].position.x -= shift;
-                }
+            charIndex++;
+            string++;
+        }
+        
+        if (alignment != TextAlignment_LEFT) {
+            float shift = caret.x - startingX;
+            if (alignment == TextAlignment_CENTER) shift /= 2;
+            const int total = charIndex;
+            for (int j = startingCharIndex; j < total; j++) {
+                textRenderer->chars[j][0].position.x -= shift;
+                textRenderer->chars[j][1].position.x -= shift;
+                textRenderer->chars[j][2].position.x -= shift;
+                textRenderer->chars[j][3].position.x -= shift;
             }
         }
     }
+    textRenderer->activeCharCount = charIndex;
 }
